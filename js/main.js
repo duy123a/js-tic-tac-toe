f622b9b82215716439122e7c11e55770699c4748
duy123a <duy123a@gmail.com>
2022-07-18 21:28:46 +0700

    init cell element list

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
+import { TURN, CELL_VALUE, GAME_STATUS } from './constants.js';
+import {
+  getCellElementList,
+  getCurrentTurnElement,
+  getCellElementAtIdx,
+  getGameStatusElement,
+} from './selectors.js';
 /**
  * Global variables
  */
-let currentTurn = 'cross';
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill('');
+
+function toggleTurn() {
+  // toggle turn
+  currentTurn = currentTurn === TURN.CROSS ? TURN.CIRCLE : TURN.CROSS;
+  // update current turn status on DOM
+  const currentTurnElement = getCurrentTurnElement();
+  if (currentTurnElement) {
+    currentTurnElement.classList.remove(TURN.CROSS, TURN.CIRCLE);
+    currentTurnElement.classList.add(currentTurn);
+  }
+}
+
+function handleCellClick(cell, index) {
+  const isClicked =
+    cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
+  if (isClicked) return;
+  // set selected cell
+  cell.classList.add(currentTurn);
+  // toggle turn
+  toggleTurn();
+
+  console.log('click', cell, index);
+}
+
+function initCellElementList() {
+  const cellElementList = getCellElementList();
+  if (!cellElementList) {
+    throw new Error("Can't find cell element list");
+  }
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener('click', () => {
+      handleCellClick(cell, index);
+    });
+  });
+}
 
 /**
  * TODOs
@@ -22,9 +64,19 @@ let cellValues = new Array(9).fill('');
  */
 
 (() => {
-  document.addEventListener('click', musicPlay);
-  function musicPlay() {
-    document.getElementById('playAudio').play();
-    document.removeEventListener('click', musicPlay);
+  // autoplay audio, only check for Chrome-based browser
+  var isChrome =
+    /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+  if (isChrome) {
+    document.querySelector('.game-page').addEventListener(
+      'click',
+      function () {
+        document.getElementById('playAudio').play();
+      },
+      { once: true }
+    );
   }
+  // bind click event for all li elements
+  initCellElementList();
+  // bind click event for replay button
 })();
